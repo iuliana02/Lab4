e2b5d2ef14dd912a2f5770a42beffb13e2b048c3
stefana-rosca <61684255+stefana-rosca@users.noreply.github.com>
2020-04-11 22:24:48 +0300

    tests

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -24,7 +24,63 @@ namespace UnitTests
 
 		TEST_CLASS(TestController)
 		{
+			TEST_METHOD(TestEmpty)
+			{
+				auto ctrl = Controller();
+				auto actual1 = ctrl.is_empty();
+				auto expected1 = true;
+				Assert::AreEqual(actual1, expected1);
+			}
+			TEST_METHOD(TestSearch)
+			{
+				Medikament med = Medikament("Nospa", 200, 35, 23.95);
+				auto ctrl = Controller();
+				auto actual1 = ctrl.search(med);
+				auto expected1 = false;
+				Assert::AreEqual(actual1, expected1);
+
+				ctrl.add(med);
+
+				auto actual2 = ctrl.search(med);
+				auto expected2 = true;
+				Assert::AreEqual(actual2, expected2);
+
+				Medikament med2 = Medikament("Ibuprofen", 80, 27, 15.95);
+				auto actual3 = ctrl.search(med2);
+				auto expected3 = false;
+				Assert::AreEqual(actual3, expected3);
+			}
+
+			TEST_METHOD(TestAddRemove)
+			{
+				auto ctrl = Controller();
+				Medikament med = Medikament("Nospa", 200, 35, 23.95);
+				ctrl.add(med);
+
+				auto actual2 = ctrl.is_empty();
+				auto expected2 = false;
+				Assert::AreEqual(actual2, expected2);
+
+				ctrl.remove(med);
+
+				auto actual3 = ctrl.is_empty();
+				auto expected3 = true;
+				Assert::AreEqual(actual3, expected3);
+
+				ctrl.undo();
+				auto actual4 = ctrl.is_empty();
+				auto expected4 = false;
+				Assert::AreEqual(actual4, expected4);
+			}
+
 			TEST_METHOD(TestModify)
+			{
+				auto ctrl = Controller();
+				Medikament med = Medikament("Nospa", 200, 35, 23.95);
+				ctrl.add(med);
+
+			}
+			/*TEST_METHOD(TestModify)
 			{
 				Medikament med = Medikament("Nurofen", 90, 20, 20.00);
 				auto ctrl = Controller();
@@ -37,7 +93,7 @@ namespace UnitTests
 				auto actual = ctrl.search(med);
 				auto expected = false;
 				Assert::AreNotEqual(actual, expected);
-			}
+			}*/
 			/*TEST_METHOD(modifyNameTest)
 			{
 				auto med = new Medikament("Nurofen", 90, 20, 20.00);
